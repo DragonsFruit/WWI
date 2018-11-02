5360af33ab8bd63582370a36e1fe82166f111d9e
Harm-jan <harmjan-wittenaar@live.nl>
2018-11-02 22:06:23 +0100

    porduct pagina verbeterd

--- Product.php
+++ Product.php
@@ -21,6 +21,21 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <title><?php echo $productNaam; ?> | WWI</title>
 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>WorldWideImports</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/shop-item.css" rel="stylesheet">
+
+    
+
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -51,12 +66,11 @@
         </div>
     </nav>
 
+<div class="container">
 
-<!--Grid Afbeelding-->
+<div class="row">
 
-    <div class ="wrapper">
-    <div>
-    <!-- Carousel -->
+  <div class="col-lg-9">
     <main class="container-fluid">
         <div class="row">
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -74,7 +88,7 @@
                                 echo "<div class='carousel-item'>";
                             }
 
-                            echo "<img class='d-block w-100' src='https://via.placeholder.com/200'/></div>";
+                            echo "<img class='d-block w-100' src='https://via.placeholder.com/900x400'/></div>";
                         }
                         
                     ?>
@@ -91,20 +105,63 @@
             </div>
         </div>
     </main>
-        <?php echo $productNaam; ?>
-    </div>
-    <div class="bestelKnop">
-        TEST
-    </div>
-    <div class="productBeschrijving">
-        <?php echo $productBeschrijving ?>
-    </div>
+  </div>
 
-    <!-- footer -->
-        <div> 
-        <footer class="footer">
-            <span class="text-muted">Place sticky footer content here.</span>
-        </div>
-    </footer>
+  <!-- /.col-lg-3 -->
+
+  <div class="col-lg-3">
+
+    <div class="card mt-4">
+    <h1 class="my-4"></h1>
+    <div class="list-group">
+    <h3 class="card-title"> <?php echo $productNaam; ?></h1>
+        <h4>$24.99</h1>
+        <p class="card-text"> <?php echo $productBeschrijving ?> </p>
+        <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
+        4.0 sterren
+    </div>
+    
+    <!-- /.card -->
+
+    <div class="card card-outline-secondary my-4">
+      <div class="card-header">
+        Product Reviews
+      </div>
+      <div class="card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+        <hr>
+        <a href="#" class="btn btn-success">Leave a Review</a>
+      </div>
+    </div>
+    <!-- /.card -->
+
+  </div>
+  <!-- /.col-lg-9 -->
+
+</div>
+
+</div>
+<!-- /.container -->
+
+<!-- Footer -->
+<footer class="py-5 bg-dark">
+<div class="container">
+  <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+</div>
+<!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
 </html>
