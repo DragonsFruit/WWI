523ff7da7a0433073dc7164bf8501404f071f22b
Harm-jan <harmjan-wittenaar@live.nl>
2018-11-02 11:24:28 +0100

    product pagina vernieuwd css is aangepast carousel toegevoegd navbar toegevoegd footer toegevoegd zoekbalk toegevoegd

--- Product.php
+++ Product.php
@@ -9,28 +9,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    
 
     <!-- Stylesheets -->
     <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <!-- JS -->
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <title><?php echo $productNaam; ?> | WWI</title>
-</head>
 
-<nav class="nav-bar">
-  <a href="#hoofdpagina" class="nav-button">Hoofdpagina</a>
-  <a href="#producten" class="nav-button">Producten</a>
-  <a href="#klantenservice" class="nav-button">Klantenservice</a>
-  <a href="#winkelmandje" class="nav-button">Winkelmandje</a>
-</nav>
+</head>
 <body>
-    <div class="productAfbeelding">
-        <?php 
-            echo '<img src="data:image/jpeg;base64,'.base64_encode($blob).'"/>';
-        ?>
-    </div>
-    <div class="productInformatie">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">WorldWideImporters</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a href="#hoofdpagina" class="nav-link">Hoofdpagina</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#producten" class="nav-link">Producten</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#klantenservice" class="nav-link">Klantenservice</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#winkelmandje" class="nav-link">Winkelmandje</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+
+    <!-- Carousel -->
+    <main class="container-fluid">
+        <div class="row">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <?php    
+                    $image = [1,2];
+                        foreach ($image as $key => $value) {
+                            if($key == 1) {
+                                echo "<div class='carousel-item active'>";
+                            } else {
+                                echo "<div class='carousel-item'>";
+                            }
+
+                            echo "<img class='d-block w-100' src='https://via.placeholder.com/200'/></div>";
+                        }
+                        
+                    ?>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+
+            </div>
+        </div>
+    </main>
+    <!-- <div class="productInformatie">
         <?php echo $productNaam; ?>
     </div>
     <div class="bestelKnop">
@@ -39,6 +96,11 @@
     <div class="productBeschrijving">
         <?php echo $productBeschrijving ?>
     </div>
-    <div>
+    <div> -->
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
 </body>
 </html>
