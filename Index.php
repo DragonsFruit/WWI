349bdbc227552e886b23e7c26668c2d5d4524d23
Harm-jan <harmjan-wittenaar@live.nl>
2018-11-14 15:13:23 +0100

    homepage improvement

--- Index.php
+++ Index.php
@@ -2,50 +2,36 @@
 	// Load classes
 	include_once "inc/autoload.php";
 
+	// Set current page active
+	$currentPage = 'home';
+
+	$headerTitle =  "wideworldimporters";
+
 	include "inc/header.php";
 ?>
 
-<h1> HOMEPAGE </h1>
-
-Dit is de indexpagina van de website
 
 
-<div class="container">
-  	<div class="row align-items-start">
-    	<div class="col">
-      		One of three columns
-    	</div>
-    	<div class="col">
-      		One of three columns
-    	</div>
-    	<div class="col">
-      		One of three columns
-    	</div>
-  	</div>
-  	<div class="row align-items-center">
-    	<div class="col">
-      		One of three columns
-    	</div>
-    	<div class="col">
-      		One of three columns
-    	</div>
-    	<div class="col">
-      		One of three columns
-    	</div>
-  	</div>
-  	<div class="row align-items-end">
-    	<div class="col">
-      		One of three columns
-    	</div>
-    	<div class="col">
-      		One of three columns
-    	</div>
-    	<div class="col">
-      		One of three columns
-    	</div>
-  	</div>
-</div>
+<main>
 
-
+	<section class="container">
+    	<div class="row">
+			<!-- Carousel -->
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  				<div class="carousel-inner">
+    				<div class="carousel-item active">
+      					<img class="d-block w-100" src="https://place-hold.it/600x500/" alt="First slide">
+    				</div>
+    				<div class="carousel-item">
+      					<img class="d-block w-100" src="https://place-hold.it/300x600/" alt="Second slide">
+    				</div>
+    				<div class="carousel-item">
+      					<img class="d-block w-100" src="https://place-hold.it/1920x1080/" alt="Third slide">
+    				</div>
+  				</div>
+			</div>
+		</div>
+    </section>
+</main>
 
 <?php include "inc/footer.php" ?>
