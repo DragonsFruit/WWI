102943a85d285a95067c8da9dec88ae2b686081f
Harm-jan <harmjan-wittenaar@live.nl>
2018-11-14 14:03:05 +0100

    start homepage

---
+++ test.php
@@ -0,0 +1,51 @@
+<?php
+	// Load classes
+	include_once "inc/autoload.php";
+
+	include "inc/header.php";
+?>
+
+<h1> HOMEPAGE </h1>
+
+Dit is de indexpagina van de website
+
+
+<div class="container">
+  	<div class="row align-items-start">
+    	<div class="col">
+      		One of three columns
+    	</div>
+    	<div class="col">
+      		One of three columns
+    	</div>
+    	<div class="col">
+      		One of three columns
+    	</div>
+  	</div>
+  	<div class="row align-items-center">
+    	<div class="col">
+      		One of three columns
+    	</div>
+    	<div class="col">
+      		One of three columns
+    	</div>
+    	<div class="col">
+      		One of three columns
+    	</div>
+  	</div>
+  	<div class="row align-items-end">
+    	<div class="col">
+      		One of three columns
+    	</div>
+    	<div class="col">
+      		One of three columns
+    	</div>
+    	<div class="col">
+      		One of three columns
+    	</div>
+  	</div>
+</div>
+
+
+
+<?php include "inc/footer.php" ?>
