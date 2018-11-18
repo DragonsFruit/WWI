d3e72cc0eac3639c2a2048c518787456dfb6fba8
kirito <casillaskhoren1@yahoo.com>
2018-11-18 20:40:57 +0100

    - added cart - added session destroy function - fixed shopping cart table - refactored whole website - refactored code - refactored design - refactored js - renamed files - fixed project structure - bunch of other stuff I forgot - fixed a fuckup

--- Product_list.php
+++ Product_list.php
@@ -43,58 +43,64 @@
 
 	include "inc/header.php";
 ?>
-<div class="container">
-    <div class="row">
-        <div class="col-12 col-sm-3">
-            <div class="card bg-light mb-3">
-                <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-list"></i> Categories</div>
-                <ul class="list-group category_block">
-                <li class="list-group-item"><a href="Product_list.php">Alle Categories</a></li>
-                    <?php foreach ($categories as $category) { ?>
-                        <li class="list-group-item"><a href="Product_list.php?categoryId=<?php echo $category["StockGroupID"]; ?>"><?php echo $category["StockGroupName"]; ?></a></li>
-                    <?php }?>   
-                </ul>
-            </div>
-        </div>
-        <div class="col">
-            <div class="row">
-                <?php foreach ($productlists as $product) {?>
-                    <div class="col-12 col-md-6 col-lg-4">
-                        <div class="card card_margin">
-                            <img class="card-img-top" style="heigth:400px width:600px" src="https://place-hold.it/600x500/" alt="Card image cap">
-                            <div class="card-body">
-                                <h4 class="card-title title_card"><a class="product-name title_card" href="product.php?id=<?php echo $product["StockItemID"]; ?>" title="View Product"><?php echo $product["StockItemName"]; ?></a></h4>
-                                <p class="card-text text_card"><?php echo $product["MarketingComments"]; ?></p>
-                                <div class="row">
-                                    <div class="price_list col-12">
-                                       <span class="price-symbol">US $</span><span class="total-price-value"><?php echo $product["RecommendedRetailPrice"]; ?></span>
-                                    </div>
-                                    <div class="col">
-                                        <a href="product.php?id=<?php echo $product["StockItemID"]; ?>" class="btn btn-primary w-100">View Product</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <?php }?>
-                <div class="col-12">
-                    <nav aria-label="product_pages">
-                        <ul class="pagination">
-                            <li class="page-item <?php if ($page == 1){echo "disabled";} ?>">
-                                <a class="page-link" href="Product_list.php?page=<?php if($page > 1){echo $page-1;} if($categoryId != 0){print "&categoryId=".$categoryId;}?>">Previous</a>
-                            </li>
-                            <?php for($i = 1; $i <= ($numberProducts / $recordsPerPage) + 1; $i++){?>
-                                <li class="page-item <?php if($i == $page){echo "active";} ?>"><a class="page-link" href="Product_list.php?page=<?php echo $i; if($categoryId != 0){print "&categoryId=".$categoryId;}?>"><?php echo $i?></a></li>
-                            <?php }?>
-                            <li class="page-item <?php if($page >= $numberProducts / $recordsPerPage){echo "disabled";} ?>">
-                                <a class="page-link" href="Product_list.php?page=<?php echo $page + 1; if($categoryId != 0){print "&categoryId=".$categoryId;}?>">Next</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </div>
+<main>
+	<div class="container">
+	    <div class="row">
+	        <div class="col-12 col-sm-3">
+	            <div class="card bg-light mb-3">
+	                <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-list"></i> Categories</div>
+	                <div class="list-group ">
+	                    <a href="Product_list.php" class="list-group-item list-group-item-action">All Categories</a>
+	                    <?php foreach ($categories as $category) { ?>
+	                        <a href="Product_list.php?categoryId=<?php echo $category["StockGroupID"]; ?>" class="list-group-item list-group-item-action"><?php echo $category["StockGroupName"]; ?></a>
+	                    <?php }?>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col">
+	            <div class="row">
+		            <div class="card-deck">
+	                <?php foreach ($productsList as $product) {?>
+	                        <div class="card mb-3" style="min-width: 250px">
+	                            <img class="card-img-top" src="https://place-hold.it/250x250/">
+	                            <div class="card-body">
+	                                <a id="productName" class="product-name" href="product.php?id=<?php echo $product["StockItemID"] ?>">
+		                                <h4 class="card-title"><?php echo $product["StockItemName"] ?></h4>
+	                                </a>
+		                            <?php if($product["MarketingComments"] != "") { ?>
+	                                    <p class="card-text"><?php echo $product["MarketingComments"] ?></p>
+		                            <?php } ?>
+	                                <div class="row">
+		                                <div class="col-12">
+			                                <a href="#!" id="buyNowButton" class="buy-now btn btn-success btn-success-custom w-100 mb-2">
+				                                <span class="price-symbol">US $</span><span id="totalPrice" ><?php echo $product["RecommendedRetailPrice"] ?></span>
+			                                </a>
+			                                <a href="#!" id="addCartButton" class="add-to-cart btn btn-primary btn-primary-custom w-100">Add to Cart</a>
+		                                </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                <?php }?>
+		            </div>
+	                <div class="col-12">
+	                    <nav aria-label="product_pages">
+	                        <ul class="pagination">
+	                            <li class="page-item <?php if ($page == 1){echo "disabled";} ?>">
+	                                <a class="page-link" href="Product_list.php?page=<?php if($page > 1){echo $page-1;} if($categoryId != 0){print "&categoryId=".$categoryId;}?>">Previous</a>
+	                            </li>
+	                            <?php for($i = 1; $i <= ($numberProducts / $recordsPerPage) + 1; $i++){?>
+	                                <li class="page-item <?php if($i == $page){echo "active";} ?>"><a class="page-link" href="Product_list.php?page=<?php echo $i; if($categoryId != 0){print "&categoryId=".$categoryId;}?>"><?php echo $i?></a></li>
+	                            <?php }?>
+	                            <li class="page-item <?php if($page >= $numberProducts / $recordsPerPage){echo "disabled";} ?>">
+	                                <a class="page-link" href="Product_list.php?page=<?php echo $page + 1; if($categoryId != 0){print "&categoryId=".$categoryId;}?>">Next</a>
+	                            </li>
+	                        </ul>
+	                    </nav>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</div>
 </main>
 
 <?php include "inc/footer.php" ?>
