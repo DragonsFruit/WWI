b523b0306e96e8948c0bc1ef9388ee317b0db856
Lorwyn <43478174+Lorwyn@users.noreply.github.com>
2018-11-23 14:47:11 +0100

    Added login & user registry

--- class/Registration.php
+++ class/Registration.php
@@ -16,4 +16,24 @@
 			$this->db->run("SET @Rows= (SELECT COUNT(*) FROM customers)");
 			$this->db->run($sql, [$name, 1, 1, 1, 1, 1, 1]);
 		}
+
+		public function registerUserPassword($email, $wachtwoord, $voornaam, $achternaam, $postcode, $straat, $huisnummer, $stad) { 
+			$hash = password_hash($wachtwoord, PASSWORD_ARGON2I);
+			$sql = "INSERT INTO useraccounts(email, wachtwoord, voornaam, achternaam, postcode, straat, huisnummer, stad)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+			$this->db->run($sql, [$email, $hash, $voornaam, $achternaam, $postcode, $straat, $huisnummer, $stad]);
+		}
 	}
+
+/* CREATE TABLE useraccounts (
+userid int NOT NULL,
+email varchar(45) NOT NULL,
+wachtwoord varchar(45) NOT NULL,
+voornaam varchar(45) NOT NULL,
+achternaam varchar(45) NOT NULL,
+gebdatum date NOT NULL,
+postcode varchar(45) NOT NULL,
+straat varchar(45) NOT NULL,
+huisnummer int NOT NULL,
+stad varchar(45) NOT NULL,
+PRIMARY KEY(UserID)) */ 
---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+    // Load classes
+    include_once "inc/autoload.php";
+
+	// Set current page active
+    $currentPage = 'login';
+    
+    $headerTitle = "Login";
+
+	include "inc/header.php";
+?>
+
+<main>
+    <div class="container">
+        <form class="form-signin">
+        <h2 class="form-signin-heading">Log in</h2><br>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" id="loginInputPassword" class="form-control" placeholder="Password" required>
+        <br><div class="checkbox">
+            <input type="checkbox" value="remember-me"> Remember me
+        </div>
+        <br><a href="registery.php">No account? Register here</a><br>
+        <br><button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+</main>
+
+<?php 
+    include "inc/footer.php";
+?>
