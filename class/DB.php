0611d99de4c12de4321ed392d2c3a9619a7db0f4
Eren Sari <Erensariaventus@hotmail.com>
2018-11-29 13:33:36 +0100

    Database beveiligd met wachtwoord

--- class/DB.php
+++ class/DB.php
@@ -1,8 +1,8 @@
 <?php
 	define('DB_HOST', '127.0.0.1');
 	define('DB_NAME', 'wideworldimporters');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
+	define('DB_USER', 'wideworldimporters');
+	define('DB_PASS', 'tXkaKYO61WUQcy6C');
 	define('DB_CHAR', 'utf8');
 
 	class DB {
