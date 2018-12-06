b8d9048e7654c6ef15959ad45c82a57ab908b987
Lorwyn <43478174+Lorwyn@users.noreply.github.com>
2018-12-06 17:43:19 +0100

    Made all fields required in registration

--- Register.php
+++ Register.php
@@ -35,41 +35,41 @@
 			<div class="form-row">
 				<div class="form-group col-md-4">
 					<label for="inputEmail">E-mail Adres</label>
-					<input type="email" class="form-control" id="inputEmail" name="email" placeholder="Voer hier uw e-mail adres in...">
+					<input type="email" class="form-control" id="inputEmail" name="email" placeholder="Voer hier uw e-mail adres in..." required>
 				</div>
 				<div class="form-group col-md-4">
 					<label for="inputPassword">Wachtwoord</label>
-					<input type="password" class="form-control" id="inputPassword" name="wachtwoord" placeholder="Voer hier uw wachtwoord in...">
+					<input type="password" class="form-control" id="inputPassword" name="wachtwoord" placeholder="Voer hier uw wachtwoord in..." required>
 				</div>
 			</div>
 			<div class="form-row">
 				<div class="form-group col-md-4">
 					<label for="inputFirstName">Voornaam</label>
-					<input type="text" class="form-control" id="inputFirstName" name="voornaam">
+					<input type="text" class="form-control" id="inputFirstName" name="voornaam" required>
 				</div>
 				<div class="form-group col-md-4">
 					<label for="inputLastName">Achternaam en Tussennaam</label>
-					<input type="text" class="form-control" id="inputLastName" name="achternaam">
+					<input type="text" class="form-control" id="inputLastName" name="achternaam" required>
 				</div>
 			</div>
 			<div class="form-row">
 				<div class="form-group col-md-6">
 					<label for="inputAddress">Adres</label>
-					<input type="text" class="form-control" id="inputAddress" name="adres">
+					<input type="text" class="form-control" id="inputAddress" name="adres" required>
 				</div>
 				<div class="form-group col-md-2">
 					<label for="inputNumber">Huisnummer</label>
-					<input type="text" class="form-control" id="inputNumber" name="nummer">
+					<input type="text" class="form-control" id="inputNumber" name="nummer" required>
 				</div>
 			</div>
 			<div class="form-row">
 				<div class="form-group col-md-6">
 					<label for="inputCity">Stad</label>
-					<input type="text" class="form-control" id="inputCity" name="stad">
+					<input type="text" class="form-control" id="inputCity" name="stad" required>
 				</div>
 				<div class="form-group col-md-2">
 					<label for="inputZip">Postcode</label>
-					<input type="text" class="form-control" id="inputZip" name="postcode">
+					<input type="text" class="form-control" id="inputZip" name="postcode" required>
 				</div>
 			</div>
 			<button type="submit" class="btn btn-primary">Registreren</button>
